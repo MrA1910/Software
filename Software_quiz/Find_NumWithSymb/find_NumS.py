34a30b7a0c309ff45eb5642d7876c0efd87f32be
MrA1910 <153997016+MrA1910@users.noreply.github.com>
2024-02-20 16:34:13 +0700

    Update find_NumS.py

--- Software_quiz/Find_NumWithSymb/find_NumS.py
+++ Software_quiz/Find_NumWithSymb/find_NumS.py
@@ -1,5 +1,6 @@
 import re
 
+
 def sum_and_show_numbers_around_symbols(file_path):
     with open(file_path, 'r') as file:
         data = [re.findall(r'\d+|[^0-9]', line.strip()) for line in file.readlines()]
@@ -12,7 +13,8 @@ def sum_and_show_numbers_around_symbols(file_path):
         for j in range(len(data[i])):
             if data[i][j] in symbols:
                 # Check the eight directions around the symbol
-                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1), (-3, -1), (-3, 1), (3, -1), (3, 1)]:
+                for dx, dy in [(-1, 0), (1, 0), (-2, 0), (2, 0), (-3, 0), (3, 0), (0, -1), (0, 1), (0, -2), (0, 2),
+                               (0, -3), (0, 3), (-3, -1), (-3, 1), (3, -2), (3, 2), (3, -3), (3, 3)]:
                     nx, ny = i + dx, j + dy
                     # Check if the new position is inside the grid and is a digit
                     if 0 <= nx < len(data) and 0 <= ny < len(data[nx]) and data[nx][ny].isdigit():
@@ -21,6 +23,7 @@ def sum_and_show_numbers_around_symbols(file_path):
                         numbers.append(num)
 
     return total, numbers
+
 
 total, numbers = sum_and_show_numbers_around_symbols('input.txt')
 print(f"The total sum of the numbers is: {total}")
